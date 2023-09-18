c1907049d05e0945fc32456402f7484d107718ee
nlukinykh <nlukinyh04@gmail.com>
2023-09-18 12:46:14 +0300

    Checked process how to work with new branch

--- src/test/java/TestStandardUserLogin.java
+++ src/test/java/TestStandardUserLogin.java
@@ -10,4 +10,6 @@ public class TestStandardUserLogin extends BaseTest {
                 "standard_user", "secret_sauce");
         Assert.assertTrue(productsPage.isProductLabelDisplayed());
     }
+
+    // create comment for new branch
 }
